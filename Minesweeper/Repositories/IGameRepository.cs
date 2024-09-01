ea6a211719fd9f336e4d9ce641be7d43371b3f08
KomaMix <ksabirov448@gmail.com>
2024-09-01 22:07:14 +0300

    Добавил класс GameService. Для него нужен IGameRepository

---
+++ Minesweeper/Repositories/IGameRepository.cs
@@ -0,0 +1,11 @@
+﻿using Minesweeper.Models;
+
+namespace Minesweeper.Repositories
+{
+    public interface IGameRepository
+    {
+        void CreateGame(Game game);
+        Game GetGame(Game game);
+        void UpdateGame(Game game);
+    }
+}
---
+++ Minesweeper/Services/GameService.cs
@@ -0,0 +1,17 @@
+﻿using Minesweeper.DTOs;
+
+namespace Minesweeper.Services
+{
+    public class GameService : IGameService
+    {
+        public GameStateDto CreateGame(int width, int height, int minesCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameStateDto MakeMove(Guid gameId, int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
