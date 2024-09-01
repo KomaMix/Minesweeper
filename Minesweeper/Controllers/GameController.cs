bcbf0d9a062af630ea78ed7956912c1d752ea8ed
KomaMix <ksabirov448@gmail.com>
2024-09-01 21:38:28 +0300

    Начальная инициализация контроллеров, и настройка Program.cs

---
+++ Minesweeper/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minesweeper.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet("new")]
+        public int New()
+        {
+            return 2;
+        }
+
+        [HttpGet("turn")]
+        public int Turn()
+        {
+            return 3;
+        }
+    }
+}
--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Не забудь потом убрать!!!!!!!!!!!!!!!!!!!
+    app.UseDeveloperExceptionPage();
+}
+
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
+
